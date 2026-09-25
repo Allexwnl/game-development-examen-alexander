c2ea3214826dfb1f823a0fdf286d7ebed3cbfb2a
alexander <alexanderzoet@gmail.com>
2026-09-25 13:17:14 +0200

    Herindeling: Balbeweging en paddlebesturing toevoegen
    Achteraf samengestelde stap uit de bestaande beginversie, met AI-hulp. Geen oorspronkelijke dagcommit; zie HERINDELING.md.

--- script/game.js
+++ script/game.js
@@ -19,8 +19,31 @@ const game = new Phaser.Game({
 function create() {
     school = this.add.rectangle(60, HEIGHT / 2, 16, 88, 0xff294d);
     student = this.add.rectangle(WIDTH - 60, HEIGHT / 2, 16, 88, 0x00ca7c);
+    ball = this.add.circle(WIDTH / 2, HEIGHT / 2, 8, 0xffffff);
+    ball.vx = 320;
+    ball.vy = 100;
+    keys = this.input.keyboard.addKeys('W,S,UP,DOWN');
 }
 
-function update() {
-    // Besturing volgt in de volgende stap.
+function update(time, delta) {
+    const seconds = Math.min(delta / 1000, 0.05);
+    movePlayer(school, keys.W, keys.S, seconds);
+    movePlayer(student, keys.UP, keys.DOWN, seconds);
+    ball.x += ball.vx * seconds;
+    ball.y += ball.vy * seconds;
+    // Tijdelijke randbotsingen; echte Pong-botsingen volgen in stap 4.
+    if (ball.x < 8 || ball.x > WIDTH - 8) {
+        ball.x = Phaser.Math.Clamp(ball.x, 8, WIDTH - 8);
+        ball.vx *= -1;
+    }
+    if (ball.y < 8 || ball.y > HEIGHT - 8) {
+        ball.y = Phaser.Math.Clamp(ball.y, 8, HEIGHT - 8);
+        ball.vy *= -1;
+    }
+}
+
+function movePlayer(paddle, up, down, seconds) {
+    if (up.isDown) paddle.y -= 340 * seconds;
+    if (down.isDown) paddle.y += 340 * seconds;
+    paddle.y = Phaser.Math.Clamp(paddle.y, 44, HEIGHT - 44);
 }
